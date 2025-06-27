29ed0453c73b9d52adfbe0bedfa050671269ed9c
Dario B. <feddar@users.noreply.github.com>
2025-06-27 20:25:17 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
-
+#
+# 1. go to https://github.com/settings/tokens
+# 2. generate new token (fine-grained) with "Self-hosted runners" permissions
+# 3. put the token in GITHUB_PAT variable in your .env file or in the docker-compose.yml file
+#
 REPO=$REPO
-REG_TOKEN=$REG_TOKEN
+#REG_TOKEN=$REG_TOKEN
 NAME=$(hostname)
+GITHUB_PAT=$GITHUB_PAT
 
 cd /home/docker/actions-runner || exit
+
+REG_TOKEN=$(curl -sX POST -H "Authorization: token $GITHUB_PAT" "https://api.github.com/orgs/$REPO/actions/runners/registration-token" | jq -r .token)
+    
 ./config.sh --url https://github.com/${REPO} --token ${REG_TOKEN} --name ${NAME}
+./config.sh --url "$RUNNER_URL" --token "$REG_TOKEN" --unattended --name "$(hostname)-runner" --replace
 
 cleanup() {
   echo "Removing runner..."
